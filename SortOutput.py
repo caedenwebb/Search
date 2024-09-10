52fc0ec767951927e99a991a5c25c8c50513670f
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-10 10:57:46 -0500

    Created a file to contain functions to sort the output before printing it to the screen

---
+++ SortOutput.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+def Alphabetize():
+    pass
+
+def SmallestToLargest():
+    pass
+
+def LargestToSmallest():
+    pass
+
+def OldestToNewest():
+    pass
+
+def NewestToOldest():
+    pass
