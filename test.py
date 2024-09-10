3c673fd2e614510b3f7f0712d1bc3b0ae596365c
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-10 01:06:12 -0500

    Test for SearchString added

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 import unittest
-
+import SearchName
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_SearchString(self):
+        print('Testing SearchString()...')
+        self.assertEqual(SearchName.SearchString("Test String", "Test"), True)
+        self.assertEqual(SearchName.SearchString("Test String", "t Str"), True)
+        self.assertEqual(SearchName.SearchString("Test String", "String"), True)
+        self.assertEqual(SearchName.SearchString("Test String", "Tef"), False)
+        self.assertEqual(SearchName.SearchString("Test String", "string"), False)
+        self.assertEqual(SearchName.SearchString("Test String", "TestString"), False)
+        self.assertEqual(SearchName.SearchString("Google Chrome.lnk", "Google"), True)
+        self.assertEqual(SearchName.SearchString("Google Drive.lnk", "Google"), True)
+        self.assertEqual(SearchName.SearchString("Discord", "cord"), True)
+
+        print('Test of SearchString() complete!')
+
 
 
 if __name__ == '__main__':
