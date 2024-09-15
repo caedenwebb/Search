3a545f69b592f4848c8f517c22e4425077bf9a52
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-15 14:38:58 -0500

    Rewrote FileClass to have more convenient structure and data

--- FileClass.py
+++ FileClass.py
@@ -1,8 +1,30 @@
 class File:
 
-    def __init__(self, filename, path, filesize='', date_created='', date_modified=''):
-        self.filename = filename
+    def __init__(self, path):
         self.path = path
-        self.filesize = filesize
-        self.date_created = date_created
-        self.date_modified = date_modified
+        self.filename = ''
+        self.size = ''
+        self.date_created = ''
+        self.date_modified = ''
+        self.ReturnData = None
+        self.Filestream = None
+
+    def ReadMode(self):
+        if (self.Filestream != None):
+            self.Filestream.close()
+            self.Filestream = None
+        self.Filestream = open(self.path, 'r')
+
+    def WriteMode(self):
+        if (self.Filestream != None):
+            self.Filestream.close()
+            self.Filestream = None
+        self.Filestream = open(self.path, 'w')
+
+    def CloseFilestream(self):
+        if (self.Filestream != None):
+            self.Filestream.close()
+            self.Filestream = None
+class Directory:
+    def __init__(self, path):
+        self.path = path
