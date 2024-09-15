17c735e08c634c1f5003f2cd55dd77107dd99bca
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-15 00:53:46 -0500

    Created class for File Objects

---
+++ FileClass.py
@@ -0,0 +1,8 @@
+class File:
+
+    def __init__(self, filename, path, filesize='', date_created='', date_modified=''):
+        self.filename = filename
+        self.path = path
+        self.filesize = filesize
+        self.date_created = date_created
+        self.date_modified = date_modified
