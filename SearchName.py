7c03ffa982f3dac799d003036249088ab43ce4ea
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-15 01:12:47 -0500

    Added comments

--- SearchName.py
+++ SearchName.py
@@ -9,28 +9,36 @@ def SearchName(SearchDir, SearchPattern) -> list:
     :return: A list of directories and files whose names match the search term
     '''
 
+    # Attempt to list the directory
     try:
         itemlist = os.listdir(SearchDir)
+    # If obtaining a list of files and directories in a directory fails because of a lack of permission, return empty list
     except PermissionError:
         return []
 
-    returnList = []
+    returnList = [] # List to contain files and directories meeting the search pattern
 
+    # Recursively search through files and directories to check if their filenames meet the search pattern
     for item in itemlist:
         # If the item is a directory
         if (os.path.isdir(SearchDir + "/" + item)):
+            # If the directory meets the search pattern, add to the returnList, and recurse through the files and directories inside said dir
             if (SearchString(item, SearchPattern) == True):
                 returnList.append(f'{SearchDir}/{item}')
                 for subitem in SearchName(SearchDir + '/' + item, SearchPattern):
                     returnList.append(subitem)
+            # If the directory does not meet the search pattern, recurse through the files and directories inside directory
             else:
                 for subitem in SearchName(SearchDir + '/' + item, SearchPattern):
                     returnList.append(subitem)
         # If the item is a file
         else:
+            # If the file meets the search pattern
             if (SearchString(item, SearchPattern) == True):
                 returnList.append(f'{SearchDir}/{item}')
+            # If the file does not meet the search pattern
             else:
                 continue
 
+    # Return the list of files and dirs meeting search pattern
     return returnList
--- search.py
+++ search.py
@@ -6,10 +6,7 @@ import time
 
 def main():
 
-    # Variables
-    SearchDir = ''
-    SearchPattern = ''
-
+    # When input is either: "search" or "python search.py"
     if (len(sys.argv) < 2):
         print('\nUsage: search [mode] [arguments]') # [directory/file] [attribute] [pattern]
         print('\nData Attributes to Search:')
@@ -19,16 +16,21 @@ def main():
         print('date-modified -------------- returns files and directories matching the date range provided in the pattern for their modification dates\n')
         sys.exit()
 
+    # When input is: "search" followed by a space and then an invalid or non-existent path.
     if (os.path.exists(sys.argv[1]) == False or os.path.isdir(sys.argv[1]) == False):
         print(f'Error: Invalid path: \'{sys.argv[1]}\' entered.\n')
-        sys.exit()
+        sys.exit() # For exiting to ensure that the following if statements do not run
 
+    # When the user fails to specify an attribute to search
     if (len(sys.argv) < 3 or sys.argv[2] == ''):
         print('Error: No attribute provided to search.\n')
-        sys.exit()
+        sys.exit() # For exiting to ensure that the following if statements do not run
 
+    # When the user specifies the filename attribute to search
     if (sys.argv[2] == 'filename'):
+        # If proper input is specified (i.e. a directory, attribute, and pattern)
         if (len(sys.argv) > 3 and sys.argv[3] != ''):
+            # Execute the search
             startTime = time.time_ns()
             filelist = SearchName.SearchName(sys.argv[1], sys.argv[3])
             endTime = time.time_ns()
@@ -39,28 +41,35 @@ def main():
             for item in filelist:
                 print(item)
             print('')
-
+        # When the user fails to specify a search pattern
         else:
             print(f'Error: No search pattern provided.\n')
 
-        sys.exit()
+        sys.exit() # For exiting to ensure that the following if statements do not run
 
     if (sys.argv[2] == 'file-size'):
-        sizeRange = sys.argv[3].split('-')
+        '''
+        Size ranges are to be specified by the user as follows: "[minimum]-[maximum]"
+        '''
+        sizeRange = sys.argv[3].split('-') # Take search argument and split it into a list with two members based on the hyphen
+
+        # Attempt to determine the maximum and minimum ranges by obtaining it from the list and converting them to integers
         try:
             minimum = sizeRange[0]
             maximum = sizeRange[1]
 
             minVal = int(sizeRange[0])
             maxVal = int(sizeRange[1])
-
+        # If the user fails to input a range correctly (e.g. failure to use a hyphen to divide minimum and maximum)
         except IndexError:
             print("Error: Input size ranges as follows: [minimum]-[maximum]")
             sys.exit()
+        # If the user inputs a range correctly, but specified anything that cannot be converted to an int (e.g. 30-15d)
         except ValueError:
             print("Error: Search only accepts integer size ranges")
-            sys.exit()
+            sys.exit() # For exiting to ensure that the following if statements do not run
 
+        # Execute the search
         startTime = time.time_ns()
         filelist = SearchFileSize.SearchFileSize(sys.argv[1], minVal, maxVal)
         endTime = time.time_ns()
@@ -71,15 +80,15 @@ def main():
         for item in filelist:
             print(item)
         print('')
-        sys.exit()
+        sys.exit() # For exiting to ensure that the following if statements do not run
 
     if (sys.argv[2] == 'date-created'):
-        sys.exit()
+        sys.exit() # For exiting to ensure that the following if statements do not run
 
     if (sys.argv[2] == 'date-modified'):
-        sys.exit()
+        sys.exit() # For exiting to ensure that the following if statements do not run
 
-
+    # When an unsupported or invalid attribute is specified
     print(f'Error: Invalid attribute: \'{sys.argv[2]}\'.')
 
 if __name__ == "__main__":
