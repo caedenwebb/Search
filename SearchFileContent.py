de6a0aff88e560711084b6930f01c7c68f2aea62
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-16 14:19:18 -0500

    Addressed errors with files that are not plain text

--- SearchFileContent.py
+++ SearchFileContent.py
@@ -7,17 +7,21 @@ def SearchFile(path, pattern) -> tuple:
     matchesPattern = False
     file = FileClass.File(path)
     file.ReturnData = []
-    file.ReadMode()
-    FileContents = file.Filestream.readlines()
-    lineNum = 1
+    try:
+        file.ReadMode()
+        FileContents = file.Filestream.readlines()
+        lineNum = 1
 
-    for line in FileContents:
-        line = line.strip('\n')
-        if (utils.SearchString(line, pattern) == True):
-            file.ReturnData.append((lineNum, line))
-            matchesPattern = True
-            lineNum = lineNum + 1
-        else:
-            lineNum = lineNum + 1
+        for line in FileContents:
+            line = line.strip('\n')
+            if (utils.SearchString(line, pattern) == True):
+                file.ReturnData.append((lineNum, line))
+                matchesPattern = True
+                lineNum = lineNum + 1
+            else:
+                lineNum = lineNum + 1
+    except:
+        print('Error: Input file not supported.')
+        file.ReturnData = []
 
     return (matchesPattern, file)
