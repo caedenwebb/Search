2e7d6b71ef311d9fe4446709eaa9dfde043a8c31
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-17 13:11:38 -0500

    Began writing CheckDate function to validate dates

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+# Python Libraries
+import sys
+# Internal Project Files
+
+# External Libraries
+
 def SearchString(SearchString, PatternString) -> bool:
     '''
     Takes in a string to search for a pattern, and returns whether the string contains the pattern
@@ -36,3 +42,60 @@ def SearchString(SearchString, PatternString) -> bool:
                 offset = offset + 1
                 continue
         return False
+
+def CheckDate(date: str):
+    '''
+    Checks a date to see if the date is a valid date; will exit the code if the date is invalid
+    :param date: Takes a date in the form (1/1/2004)
+    :return: None
+    '''
+
+    if (int(date.split('/')[2]) <= 1969):
+        print(f'\nError: \'{date}\' is invalid because Search does not support dates prior to 1/1/1970.')
+        sys.exit()
+    elif (int(date.split('/')[0]) <= 0 or int(date.split('/')[0]) >= 13):
+        print(f'\nError: \'{date}\' is not a valid date.')
+        sys.exit()
+    else:
+        # Months other than February
+        if (date.split('/')[0] == '1'):
+            if (int(date.split('/')[1]) <= 0 or int(date.split('/')[1]) >= 32):
+                print(f'\nError: \'{date}\' is not a valid date because \'{date.split('/')[1]}\' is not in the range of days in the month of January (1-31).\n')
+                sys.exit()
+        elif (date.split('/')[0] == '3'):
+            if (int(date.split('/')[1]) <= 0 or int(date.split('/')[1]) >= 32):
+                print(f'\nError: \'{date}\' is not a valid date because \'{date.split('/')[1]}\' is not in the range of days in the month of March (1-31).\n')
+                sys.exit()
+        elif (date.split('/')[0] == '4'):
+            if (int(date.split('/')[1]) <= 0 or int(date.split('/')[1]) >= 31):
+                print(f'\nError: \'{date}\' is not a valid date because \'{date.split('/')[1]}\' is not in the range of days in the month of April (1-30).\n')
+                sys.exit()
+        elif (date.split('/')[0] == '5'):
+            if (int(date.split('/')[1]) <= 0 or int(date.split('/')[1]) >= 32):
+                print(f'\nError: \'{date}\' is not a valid date because \'{date.split('/')[1]}\' is not in the range of days in the month of May (1-31).\n')
+                sys.exit()
+        elif (date.split('/')[0] == '6'):
+            if (int(date.split('/')[1]) <= 0 or int(date.split('/')[1]) >= 31):
+                print(f'\nError: \'{date}\' is not a valid date because \'{date.split('/')[1]}\' is not in the range of days in the month of June (1-30).\n')
+                sys.exit()
+        elif (date.split('/')[0] == '7'):
+            if (int(date.split('/')[1]) <= 0 or int(date.split('/')[1]) >= 32):
+                print(f'\nError: \'{date}\' is not a valid date because \'{date.split('/')[1]}\' is not in the range of days in the month of July (1-31).\n')
+                sys.exit()
+        elif (date.split('/')[0] == '8'):
+            if (int(date.split('/')[1]) <= 0 or int(date.split('/')[1]) >= 32):
+                print(f'\nError: \'{date}\' is not a valid date because \'{date.split('/')[1]}\' is not in the range of days in the month of August (1-31)\n.')
+                sys.exit()
+        elif (date.split('/')[0] == '9'):
+            pass
+        elif (date.split('/')[0] == '10'):
+            pass
+        elif (date.split('/')[0] == '11'):
+            pass
+        elif (date.split('/')[0] == '12'):
+            pass
+        # February
+        else:
+            pass
+def CheckDateRange(date: str):
+    pass
