d57174aaa694df99d3145c8e7e46c1bab365333b
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-22 22:22:53 -0500

    Reorganized AttributeSearch module

--- AttributeSearch.py
+++ AttributeSearch.py
@@ -33,70 +33,11 @@ def AttributeSearch():
 
     # When the user specifies the filename attribute to search
     if (sys.argv[3] == 'filename'):
-        # If proper input is specified (i.e. a directory, attribute, and pattern)
-        if (len(sys.argv) > 4 and sys.argv[4] != ''):
-            # Execute the search
-            startTime = time.time_ns()
-            filelist = SearchName.SearchName(sys.argv[2], sys.argv[4])
-            endTime = time.time_ns()
-            timeUsed = endTime - startTime
-
-            # Print out file list
-            print(f'\nResults ({len(filelist)} results, {timeUsed}ns):\n')
-            for item in filelist:
-                print(item)
-            print('')
-        # When the user fails to specify a search pattern
-        else:
-            print(f'Error: No search pattern provided.\n')
-
+        FileNameSearch()
         sys.exit()  # For exiting to ensure that the following if statements do not run
 
     if (sys.argv[3] == 'file-size'):
-        # If the user does not provide a size range
-        if (len(sys.argv) < 5):
-            print('Error: No size range provided.\n')
-            sys.exit()
-        '''
-        Size ranges are to be specified by the user as follows: "[minimum]-[maximum]"
-        '''
-        sizeRange = sys.argv[4].split('-')  # Take search argument and split it into a list with two members based on the hyphen
-
-        # Attempt to determine the maximum and minimum ranges by obtaining it from the list and converting them to integers
-        try:
-            minimum = sizeRange[0]
-            maximum = sizeRange[1]
-
-            minVal = int(sizeRange[0])
-            maxVal = int(sizeRange[1])
-
-        # If the user fails to input a range correctly (e.g. failure to use a hyphen to divide minimum and maximum)
-        except IndexError:
-            print("Error: Input size ranges as follows: [minimum]-[maximum]\n")
-            sys.exit()
-
-        # If the user inputs a range correctly, but specified anything that cannot be converted to an int (e.g. 30-15d)
-        except ValueError:
-            print("Error: Search only accepts integer size ranges\n")
-            sys.exit()  # For exiting to ensure that the following if statements do not run
-
-
-        # Check if the size range minimum is smaller than or equal to the maximum:
-        if (minVal > maxVal):
-            print(f"\nError: Search minimum '{minVal}' is greater than search maximum '{maxVal}'. Did you mean '{maxVal}-{minVal}?'\n")
-            sys.exit()
-
-        # Execute the search
-        startTime = time.time_ns()
-        res = SearchFileSize.SearchFileSize(sys.argv[2], minVal, maxVal)
-        endTime = time.time_ns()
-        timeUsed = endTime - startTime
-
-        # Print out file list
-        print(f'\nResults ({len(res[0])} results, {timeUsed}ns):\n')
-        for item in res[0]:
-            print(item)
-        print('')
+        FileSizeSearch()
         sys.exit()  # For exiting to ensure that the following if statements do not run
 
     if (sys.argv[3] == 'date-created'):
@@ -121,3 +62,68 @@ def AttributeSearch():
 
     # When an unsupported or invalid attribute is specified
     print(f'Error: Invalid attribute: \'{sys.argv[3]}\'.')
+
+def FileNameSearch():
+    # If proper input is specified (i.e. a directory, attribute, and pattern)
+    if (len(sys.argv) > 4 and sys.argv[4] != ''):
+        # Execute the search
+        startTime = time.time_ns()
+        filelist = SearchName.SearchName(sys.argv[2], sys.argv[4])
+        endTime = time.time_ns()
+        timeUsed = endTime - startTime
+
+        # Print out file list
+        print(f'\nResults ({len(filelist)} results, {timeUsed}ns):\n')
+        for item in filelist:
+            print(item)
+        print('')
+    # When the user fails to specify a search pattern
+    else:
+        print(f'Error: No search pattern provided.\n')
+
+def FileSizeSearch():
+    # If the user does not provide a size range
+    if (len(sys.argv) < 5):
+        print('Error: No size range provided.\n')
+        sys.exit()
+    '''
+    Size ranges are to be specified by the user as follows: "[minimum]-[maximum]"
+    '''
+    sizeRange = sys.argv[4].split(
+        '-')  # Take search argument and split it into a list with two members based on the hyphen
+
+    # Attempt to determine the maximum and minimum ranges by obtaining it from the list and converting them to integers
+    try:
+        minimum = sizeRange[0]
+        maximum = sizeRange[1]
+
+        minVal = int(sizeRange[0])
+        maxVal = int(sizeRange[1])
+
+    # If the user fails to input a range correctly (e.g. failure to use a hyphen to divide minimum and maximum)
+    except IndexError:
+        print("Error: Input size ranges as follows: [minimum]-[maximum]\n")
+        sys.exit()
+
+    # If the user inputs a range correctly, but specified anything that cannot be converted to an int (e.g. 30-15d)
+    except ValueError:
+        print("Error: Search only accepts integer size ranges\n")
+        sys.exit()  # For exiting to ensure that the following if statements do not run
+
+    # Check if the size range minimum is smaller than or equal to the maximum:
+    if (minVal > maxVal):
+        print(
+            f"\nError: Search minimum '{minVal}' is greater than search maximum '{maxVal}'. Did you mean '{maxVal}-{minVal}?'\n")
+        sys.exit()
+
+    # Execute the search
+    startTime = time.time_ns()
+    res = SearchFileSize.SearchFileSize(sys.argv[2], minVal, maxVal)
+    endTime = time.time_ns()
+    timeUsed = endTime - startTime
+
+    # Print out file list
+    print(f'\nResults ({len(res[0])} results, {timeUsed}ns):\n')
+    for item in res[0]:
+        print(item)
+    print('')
