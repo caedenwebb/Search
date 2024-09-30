47921d6e8baa6685bb33082ef975d880dfa054ac
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-09-30 13:33:15 -0500

    Updated tests for TestDateForMatch function

--- test.py
+++ test.py
@@ -19,7 +19,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_SearchDateCreated_TestDateForMatch(self):
         print('Testing TestDateForMatch()...')
-        self.assertEqual(SearchDateCreated.TestDateForMatch([3, 2, 2015], ['3/2/2015']), True)
+        self.assertEqual(SearchDateCreated.TestDateForMatch([4, 3, 2015], ['4/3/2015-4/5/2017']), True)
+        self.assertEqual(SearchDateCreated.TestDateForMatch([3, 8, 2019], ['1/1/1970-3/9/2019']), True)
+        self.assertEqual(SearchDateCreated.TestDateForMatch([3, 8, 2019], ['4/3/2020-4/18/2025']), False)
+        self.assertEqual(SearchDateCreated.TestDateForMatch([9, 4, 2016], ['9/5/2015-9/5/2016']), True)
+        self.assertEqual(SearchDateCreated.TestDateForMatch([2, 19, 2024], ['3/15/2024-9/30/2024']), False)
         print('Test of TestDateForMatch() complete!')
 
 if __name__ == '__main__':
