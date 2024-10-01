fc062bd6398d99ed2b213eab30c577fe73fee336
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2024-10-01 12:54:49 -0500

    Wrote quicksort to sort files based on how old or young they are

--- SortOutput.py
+++ SortOutput.py
@@ -70,15 +70,57 @@ def LargestToSmallest(filelist: list):
     return finalList
 
 def OldestToNewest(filelist: list):
-    '''
-    Sorts files based on their dates of creation from oldest to newest
-    :param filelist:
-    :return:
-    '''
 
+    # Base Cases
+    if (len(filelist) == 0):
+        return filelist
+
+    if (len(filelist) == 1):
+        return filelist
+
+    if (len(filelist) == 2):
+        if (filelist[0].rawCreationTime > filelist[1].rawCreationTime):
+            return [filelist[1], filelist[0]]
+        else:
+            return [filelist[0], filelist[1]]
+
+    # Find pivot
+    pivot = filelist[0]
+
+    # Create lists of values less than pivot
+    lesserValues = []
+    equalValues = []
+    greaterValues = []
+
+    for value in filelist:
+        if (value.rawCreationTime < pivot.rawCreationTime):
+            lesserValues.append(value)
+        elif (value.rawCreationTime == pivot.rawCreationTime):
+            equalValues.append(value)
+        else:
+            greaterValues.append(value)
+
+    # Create lists of values lesser than the pivot
+    sortedLesserValues = OldestToNewest(lesserValues)
+
+    # Recursively sort values greater than pivot
+    sortedGreaterValues = OldestToNewest(greaterValues)
+
+    # Merge
+    returnList = sortedLesserValues + equalValues + sortedGreaterValues
+
+    return returnList
 def NewestToOldest(filelist: list):
     '''
     Sorts files based on their dates of creation from newest to oldest
     :param filelist:
     :return:
     '''
+    sortedFileList = OldestToNewest(filelist)
+    finalFileList = []
+    i = len(sortedFileList)-1
+    while (i >= 0):
+        finalFileList.append(sortedFileList[i])
+        i = i - 1
+
+    return finalFileList
